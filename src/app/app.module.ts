1a9866ab159f85c56973006440cc295a58004a26
emiranda <ernestolebni123@gmail.com>
2022-06-07 00:03:48 -0600

    cambios de configuracion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { environment } from 'src/environments/environment';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
@@ -8,6 +10,11 @@ import { FooterComponent } from './common/footer/footer.component';
 import { AuthComponent } from './components/auth/auth.component';
 import { PresentationComponent } from './components/presentation/presentation.component';
 import { APP_ROUTING } from './app.routes';
+// Angular
+import {AngularFireModule} from "@angular/fire/compat"
+import {AngularFirestoreModule} from "@angular/fire/compat/firestore"
+
+
 
 @NgModule({
   declarations: [
@@ -20,7 +27,9 @@ import { APP_ROUTING } from './app.routes';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    APP_ROUTING
+    APP_ROUTING,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCd6nsb5MV7coaTi5CdagullkAjPmCmf9Q",
+    authDomain: "ferro-44a.firebaseapp.com",
+    projectId: "ferro-44a",
+    storageBucket: "ferro-44a.appspot.com",
+    messagingSenderId: "443900906345",
+    appId: "1:443900906345:web:b9fab9fefc6ab3b412cbf8",
+    measurementId: "G-EDWSKGJH84"
+  }
 };
 
 /*
